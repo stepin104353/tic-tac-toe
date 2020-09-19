091c672d3aa1f2cf282ae0e819881230c964b6fd
stepin104353 <69048783+stepin104353@users.noreply.github.com>
2020-09-19 13:22:08 +0530

    Add files via upload

---
+++ src.c
@@ -0,0 +1,66 @@
+int checkwin()
+{
+    if (square[1] == square[2] && square[2] == square[3])
+        return 1;
+
+    else if (square[4] == square[5] && square[5] == square[6])
+        return 1;
+
+    else if (square[7] == square[8] && square[8] == square[9])
+        return 1;
+
+    else if (square[1] == square[4] && square[4] == square[7])
+        return 1;
+
+    else if (square[2] == square[5] && square[5] == square[8])
+        return 1;
+
+    else if (square[3] == square[6] && square[6] == square[9])
+        return 1;
+
+    else if (square[1] == square[5] && square[5] == square[9])
+        return 1;
+
+    else if (square[3] == square[5] && square[5] == square[7])
+        return 1;
+
+    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
+        square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7]
+        != '7' && square[8] != '8' && square[9] != '9')
+
+        return 0;
+    else
+        return  - 1;
+}
+
+
+/*******************************************************************
+FUNCTION TO DRAW BOARD OF TIC TAC TOE WITH PLAYERS MARK
+ ********************************************************************/
+char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+
+void board()
+{
+    system("cls");
+    printf("\n\n\tTic Tac Toe\n\n");
+
+    printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
+
+
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n", square[1], square[2], square[3]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", square[4], square[5], square[6]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", square[7], square[8], square[9]);
+
+    printf("     |     |     \n\n");
+}
+
